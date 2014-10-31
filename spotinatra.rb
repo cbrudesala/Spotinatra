3ed20de7572d30413589b78b7091bbd83d7a2751
Cecilia <Cecilia@MacBook-Pro-de-Cecilia.local>
2014-10-31 13:23:16 +0100

    working fine

--- spotinatra.rb
+++ spotinatra.rb
@@ -1,11 +1,29 @@
 require 'sinatra'
 require "sinatra/reloader"
-require 'artii'
 
-get '/artii/:sentence' do
-  @sentence = params["sentence"]
-  x = Artii::Base.new
-  @asciiSentence = x.asciify(@sentence)
+@@SONG_LIST = []
+
+get '/' do
 	erb :index
 end
+
+post '/songs/new' do
+	@name = params["name"]
+	@artist = params["artist"]
+	if (@name == "" || @artist == "")
+		redirect "/error"
+	end
+  @new_song = [@name, @artist]
+  @@SONG_LIST.push(@new_song)
+  redirect "/"
+end
+
+get '/enough' do
+	"IS WORTH F**ING NOTHING"
+end
+
+get '/error' do
+	"You've missed something, go back and try again!"
+end
+
 
